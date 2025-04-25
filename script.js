e7705e3048cba3a3dc1e92c2e8c8517175a5fcdf
Yogesh Summan <yogeshsumman2001@gmail.com>
2025-04-26 01:30:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function removeColor() {
+    // Get the select element
+    let select = document.getElementById("colorSelect");
+    
+    // Remove the selected option
+    if (select.selectedIndex !== -1) {
+        select.remove(select.selectedIndex);
+    }
+}
